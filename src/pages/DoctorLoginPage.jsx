125aa83c44a9fc9595a2d9af14d09a90862d1df5
rentu1970 <rentu1970@gmail.com>
2025-03-21 12:11:25 +0000

    Doctor auth ui

---
+++ src/pages/DoctorLoginPage.jsx
@@ -0,0 +1,12 @@
+import { DoctorLoginForm } from "@/components/DoctorLoginForm";
+
+
+export default function DoctorLoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <DoctorLoginForm/>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/DoctorRegistrationPage.jsx
@@ -0,0 +1,12 @@
+import { DoctorRegistrationFrom } from "@/components/DoctorRegistrationForm";
+
+
+export default function DoctorRegistrationPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <DoctorRegistrationFrom/>
+      </div>
+    </div>
+  )
+}
